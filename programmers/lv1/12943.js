a09234cc2fd116cb05b3941fbb5346641a3417f1
Kang Jinhee <20wlsgml@gmail.com>
2023-03-04 18:24:27 +0900

    solved: 12943(85%)

---
+++ programmers/lv1/12943.js
@@ -0,0 +1,30 @@
+/**
+ * @title 콜라츠 추측
+ * @description
+ * 주어진 수가 1이 될 때까지
+ * 입력된 수가 짝수라면 2로 나눕니다.
+ * 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+ * 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다.
+ *
+ * 작업을 몇 번이나 반복해야 하는지 반환하는 함수, solution을 완성해 주세요.
+ * 단, 주어진 수가 1인 경우에는 0을, 작업을 500번 반복할 때까지 1이 되지 않는다면 –1을 반환
+ * @restriction num은 1 이상 8,000,000 미만인 정수
+ *
+ * @param {number} num
+ * @timecomplexity O(logn)
+ */
+function solution(num) {
+  if (num === 1) return 0;
+  let count = 0;
+
+  do {
+    if (num === 1) break;
+
+    count++;
+    num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+  } while (count < 500);
+
+  return count === 500 ? -1 : count;
+}
+
+solution(6);
