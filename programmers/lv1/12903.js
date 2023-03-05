e2dd9bcf84ea87593b7b871c26100932b076f29c
Kang Jinhee <20wlsgml@gmail.com>
2023-03-05 11:22:47 +0900

    solved: 12903(84%)
    가운데 글자 가져오기
    Math.trunc() = 숫자 표현식을 받아 소수 제거

---
+++ programmers/lv1/12903.js
@@ -0,0 +1,17 @@
+/**
+ * @title
+ * @description
+ * 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요.
+ * 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+ * @restriction s는 길이가 1 이상, 100이하인 스트링입니다.
+ *
+ * @param {string} s
+ * @complexity O(1)
+ */
+function solution(s) {
+  return s.length % 2 === 0
+    ? s.substring(Math.trunc(s.length / 2) - 1, Math.trunc(s.length / 2) + 1)
+    : s.substring(Math.trunc(s.length / 2), Math.trunc(s.length / 2) + 1);
+}
+
+solution("가나다라마바사아");
