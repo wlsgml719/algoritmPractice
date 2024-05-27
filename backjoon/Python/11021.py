4f3a0f7b2004c48f77f430b3e052f19dae7ad121
Kang Jinhee <20wlsgml@gmail.com>
2024-05-27 20:41:19 +0900

    unsolved: 11021(49%)

---
+++ backjoon/Python/11021.py
@@ -0,0 +1,23 @@
+'''
+    제목: A+B - 7
+    설명: 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+    입력: 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+    각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+    출력: 각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+'''
+count = int(input())
+numbers = []
+
+for i in range(count):
+    first, second = map(int, input().split())
+    numbers.append(first)
+    numbers.append(second)
+
+for i in range(count):
+    index = 2*i
+    print("Case #{}: ".format(i+1), numbers[index] + numbers[index+1])
+
+
+''' (fail) 출력형식이 잘못되었습니다.
+    코드길이: 267 B
+'''
